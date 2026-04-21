724226275d1135f5893a02feb79848878a8878a3
vionaldi <vionaldi@gmail.com>
2026-04-21 16:04:18 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+const SCRIPT_URL =
+  'https://script.google.com/macros/s/AKfycbzwvSrKaCPw1kvD0ENwUpuGzLZRULOvJAxHVK7uoDN3UMQtavFasHfuaUgiU0FZebnj-w/exec'
 
-  return {
-    plugins: [react()],
-
-    server: {
-      host: true,
-      allowedHosts: ['qrform.smfenterprise.com', 'www.qrform.smfenterprise.com'],
-      proxy: {
-        '/api': {
-          target: env.VITE_API_TARGET,
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['qrform.smfenterprise.com', 'www.qrform.smfenterprise.com'],
+    proxy: {
+      '/api': {
+        target: SCRIPT_URL,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-
-    build: {
-      rollupOptions: {
-        external: ['@zxing/library'],
-      },
-    },
-  }
+  },
 })
